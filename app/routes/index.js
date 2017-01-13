634ef007b6c12bd1acfc6c01403dc6961fa831bb
Kenneth Black <kenneth.black@live.com>
2017-01-13 07:15:20 -0800

    add question update and delete functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
+  },
+  actions: {
+    saveQuestion(params) {
+      this.store.createRecord('question', params).save();
+      this.transitionTo('index');
+    }
   }
 });
